0654685fde993c5bd45de2b84129b5a505448ed7
mrgopalsharma <72728990+mrgopalsharma@users.noreply.github.com>
2025-04-16 17:04:00 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#Task1
+num1=input('Enter First Number: ')
+num2=input('Enter Second Number: ')
+num1=int(num1)
+num2=int(num2)
+
+a=num1+num2
+s=num1-num2
+m=num1*num2
+d=num1/num2
+
+print('Addition: ', a)
+print('Subtract: ', s)
+print('Multiply: ', m)
+print('Divide: ', d)
+
+
+#Task2
+fname= input("Enter Your first name: ")
+lname= input("Enter Your Last name: ")
+fullname=(fname+ " " +lname)
+print("Hello, ", fullname,'!', 'Welcome to the Python program' )
